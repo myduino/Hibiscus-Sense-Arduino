de76f4e1be38419af2db2b1893f76414752e994f
Mohamad Ariffin Zulkifli <ariffinastute@live.com>
2021-02-04 13:46:00 +0800

    update comments

--- Exercise_02_Glowing_Light/Exercise_02_Glowing_Light.ino
+++ Exercise_02_Glowing_Light/Exercise_02_Glowing_Light.ino
@@ -12,20 +12,20 @@
  */
 
 void setup() {
-  // configure PWM controller congfiguration
+  // configure PWM controller congfiguration.
   ledcSetup(0, 5000, 8);
-  // declare the GPIO number for PWM signal output
+  // declare the GPIO number for PWM signal output.
   ledcAttachPin(2, 0);
 }
 
 void loop() {
   // function for() to create decremental value by 1 start from 255 --> 0
-  // from OFF LED to linear increasing brightness, for active-low circuit
+  // from OFF LED to linear increasing brightness, for active-low circuit.
   for(int brightness = 255; brightness >= 0; brightness--){   
-    // generate PWM output signal according to variable brightness value
+    // generate PWM output signal according to variable brightness value.
     ledcWrite(0, brightness);
     delay(15);
   }
-  // wait for 0.2 seconds before start again
+  // wait for 0.2 seconds before start again.
   delay(200);
 }
