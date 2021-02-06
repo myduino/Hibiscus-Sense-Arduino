a7ec7acc65819eac7963325c7661fc7d1f8913fd
Mohamad Ariffin Zulkifli <ariffinastute@live.com>
2021-02-06 20:10:19 +0800

    update sketch

--- Exercise_02_Glowing_Light/Exercise_02_Glowing_Light.ino
+++ Exercise_02_Glowing_Light/Exercise_02_Glowing_Light.ino
@@ -22,7 +22,9 @@ void loop() {
   // function for() to create decremental value by 1 start from 255 --> 0
   // from OFF LED to linear increasing brightness, for active-low circuit.
   for(int brightness = 255; brightness >= 0; brightness--){   
-    // generate PWM output signal according to variable brightness value.
+    // ledcWrite() function will generate PWM output signal according to variable brightness value
+    // 1st argument: PWM channel number.
+    // 2nd argument: Tone frequency.
     ledcWrite(0, brightness);
     delay(15);
   }
