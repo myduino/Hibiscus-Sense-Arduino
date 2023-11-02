ed0fe2c27be08d19c8165a194b0e8969a523131c
ariffinzulkifli <ariffinastute@live.com>
2023-11-02 17:57:21 +0800

    update code

--- Hibiscus_Sense_QC/Hibiscus_Sense_QC.ino
+++ Hibiscus_Sense_QC/Hibiscus_Sense_QC.ino
@@ -7,9 +7,9 @@
 #include <MQTT.h>
 #include <Wire.h>
 
-#define WIFI_SSID                 "myinvententerprise"
-#define WIFI_PASSWORD             "04222682"
-#define MQTT_HOST                 "broker.hivemq.com"
+#define WIFI_SSID                 "YourWiFiSSID"
+#define WIFI_PASSWORD             "YourWiFiPassword"
+#define MQTT_HOST                 "MQTTBrokerDNSorIP"
 #define MQTT_PUBLISH_TOPIC        "60177875232/Hibiscus-Sense"
 #define MQTT_CONTROL_RGB_TOPIC    "60177875232/Hibiscus-Sense/RGB"
 #define MQTT_CONTROL_LED_TOPIC    "60177875232/Hibiscus-Sense/LED"
