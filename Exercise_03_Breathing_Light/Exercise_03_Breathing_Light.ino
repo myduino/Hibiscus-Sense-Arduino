15380c8ad4d81c557ec6f76b2786c75ac9aac6a9
ariffinzulkifli <ariffinastute@live.com>
2024-04-19 15:56:10 +0800

    update sketch

--- Exercise_03_Breathing_Light/Exercise_03_Breathing_Light.ino
+++ Exercise_03_Breathing_Light/Exercise_03_Breathing_Light.ino
@@ -19,7 +19,7 @@ void setup() {
 }
 
 void loop() {
-  // function for() to create decremental value by 1 start from 255 --> 0
+  // for() statement to create decremental value by 1 start from 255 --> 0
   // from OFF LED to linear increasing brightness, for active-low circuit
   for(int brightness = 255; brightness >= 0; brightness--){   
     ledcWrite(0, brightness);
@@ -28,7 +28,7 @@ void loop() {
   // wait for 0.2 seconds
   delay(200);
   
-  // function for() to create incremental value by 1 start from 0 --> 255
+  // for() statement to create incremental value by 1 start from 0 --> 255
   // from ON LED to linear decreasing brightness, for active-low circuit
   for(int brightness = 0; brightness <= 255; brightness++){   
     ledcWrite(0, brightness);
