26dc8a6854c68f74004110fe942f5c981bbe996c
ariffinzulkifli <ariffinastute@live.com>
2024-04-21 04:21:46 +0800

    add new exercise

---
+++ Exercise_14_IoT_FavoriotMQTTS/Exercise_14_IoT_FavoriotMQTTS.ino
@@ -0,0 +1,205 @@
+/*
+ * Hibiscus Sense - Exercise 14 IoT using Favoriot MQTTS
+ * 
+ * Author: Mohamad Ariffin Zulkifli
+ * Organization: Myinvent Technologies Sdn Bhd
+ *
+ * This sketch has 4 summary execution steps:
+ * 1. Initialized Wi-Fi conectivity
+ * 2. Initialized MQTT connection to Favoriot MQTT broker
+ * 2. Data Acquisition - Read data from the sensors
+ * 3. Data Ingestion - Send data to Favoriot's data stream using secure MQTT connection
+ *
+ * Select the board as ESP32 Dev Module before compiling the sketch
+ * (example) Go to menu, Tools > Board > esp32 > ESP32 Dev Module
+ *
+ * Favoriot's Secure Certificate Validity Expires On:
+ * Saturday, 26 April 2025 at 08:54:18
+ * 
+ */
+
+#include <WiFiClientSecure.h>
+#include <MQTT.h>
+#include <Adafruit_APDS9960.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_NeoPixel.h>
+#include "FavoriotCA.h"
+
+const char ssid[] = "YOUR_WIFI_SSID";
+const char password[] = "YOUR_WIFI_PASSWORD";
+const char deviceDeveloperId[] = "YOUR_DEVICE_DEVELOPER_ID";
+const char deviceAccessToken[] = "YOUR_DEVICE_ACCESS_TOKEN";
+const char publishTopic[] = "/v2/streams";
+const char statusTopic[] = "/v2/streams/status";
+
+Adafruit_NeoPixel rgb(1, 16);
+
+Adafruit_APDS9960 apds;
+Adafruit_BME280 bme;
+Adafruit_MPU6050 mpu;
+
+sensors_event_t a, g, temp;
+
+unsigned long lastMillis = 0;
+
+WiFiClientSecure net;
+MQTTClient mqtt(4096);
+
+void connectToWiFi() {
+  Serial.print("Connecting to Wi-Fi '" + String(ssid) + "' ...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println(" connected!");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("Incoming Status: " + payload);
+  Serial.println();
+}
+
+void connectToFavoriotMQTT() {
+  Serial.print("Connecting to Favoriot MQTT ...");
+
+  net.setCACert(rootCACertificate);
+
+  mqtt.begin("mqtt.favoriot.com", 8883, net);
+  mqtt.onMessage(messageReceived);
+
+  String uniqueString = String(ssid) + "-" + String(random(1, 98)) + String(random(99, 999));
+  char uniqueClientID[uniqueString.length() + 1];
+
+  uniqueString.toCharArray(uniqueClientID, uniqueString.length() + 1);
+
+  while (!mqtt.connect(uniqueClientID, deviceAccessToken, deviceAccessToken)) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println(" connected!");
+
+  Serial.println("Subscribe to: " + String(deviceAccessToken) + String(statusTopic));
+
+  mqtt.subscribe(String(deviceAccessToken) + String(statusTopic));
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  rgb.begin();
+  rgb.show();
+
+  if (!apds.begin()) {
+    Serial.println("Failed to find Hibiscus Sense APDS9960 chip");
+  }
+
+  apds.enableProximity(true);
+
+  if (!bme.begin()) {
+    Serial.println("Failed to find Hibiscus Sense BME280 chip");
+  }
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find Hibiscus Sense MPU6050 chip");
+  }
+
+  Serial.println();
+
+  // STEP 1: Initialized Wi-Fi conectivity
+  connectToWiFi();
+  // STEP 2: Initialized MQTT connection to Favoriot MQTT broker
+  connectToFavoriotMQTT();
+}
+
+void loop() {
+  mqtt.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+  }
+
+  if (!mqtt.connected()) {
+    connectToFavoriotMQTT();
+  }
+
+  // STEP 3: Data Acquisition - Read data from the sensors
+  Serial.print("Proximity: ");
+  Serial.println(apds.readProximity());
+
+  Serial.print("Relative Humidity: ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %RH");
+
+  Serial.print("Approx. Altitude: ");
+  Serial.print(bme.readAltitude(1013.25));
+  Serial.println(" m");
+
+  Serial.print("Barometric Pressure: ");
+  Serial.print(bme.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Ambient Temperature: ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
+
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("Acceleration X:");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y:");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z:");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X:");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y:");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z:");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  // STEP 4: Data Ingestion - Send data to Favoriot's data stream using secure MQTT connection
+  // Interval 15 seconds
+  if (millis() - lastMillis > 15000) {
+    lastMillis = millis();
+
+    String favoriotJson = "{\"device_developer_id\":\"" + String(deviceDeveloperId) + "\",\"data\":{";
+    
+    favoriotJson += "\"proximity\":\"" + String(apds.readProximity()) + "\",";
+    favoriotJson += "\"humidity\":\"" + String(bme.readHumidity()) + "\",";
+    favoriotJson += "\"altitude\":\"" + String(bme.readAltitude(1013.25)) + "\",";
+    favoriotJson += "\"barometer\":\"" + String(bme.readPressure()/100.00) + "\",";
+    favoriotJson += "\"temperature\":\"" + String(bme.readTemperature()) + "\",";
+    
+    mpu.getEvent(&a,&g,&temp);
+    
+    favoriotJson += "\"accx\":\"" + String(a.acceleration.x) + "\",";
+    favoriotJson += "\"accy\":\"" + String(a.acceleration.y) + "\",";
+    favoriotJson += "\"accz\":\"" + String(a.acceleration.z) + "\",";
+    favoriotJson += "\"gyrox\":\"" + String(g.gyro.x) + "\",";
+    favoriotJson += "\"gyroy\":\"" + String(g.gyro.y) + "\",";
+    favoriotJson += "\"gyroz\":\"" + String(g.gyro.z) + "\"";
+    
+    favoriotJson += "}}";
+
+    Serial.println("\nSending data to Favoriot's Data Stream ...");
+
+    Serial.println("Data to Publish: " + favoriotJson);
+    Serial.println("Publish to: " + String(deviceAccessToken) + String(publishTopic));
+
+    mqtt.publish(String(deviceAccessToken) + String(publishTopic), favoriotJson);
+  }
+
+  Serial.println("=============================================");
+  delay(3000);
+}
